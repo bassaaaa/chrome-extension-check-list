6054857ad5cd02892b297beba9b9a06a8002f41f
bassaaaa <white.galaxys2.album2@gmail.com>
2024-04-26 20:57:10 +0900

    titleを変数化

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
 import { ToggleWithLabel } from "./components/ToggleWithLabel";
 
-const items = [
-  {
-    id: 1,
-    label: "チェック項目1",
-    tip: "補足事項1",
-  },
-  {
-    id: 2,
-    label: "チェック項目2",
-    tip: "補足事項2",
-  },
-  {
-    id: 3,
-    label: "チェック項目3",
-    tip: "補足事項3",
-  },
-];
+const checkList = {
+  title: "チェックリスト",
+  items: [
+    {
+      id: 1,
+      label: "チェック項目1",
+      tip: "補足事項1",
+    },
+    {
+      id: 2,
+      label: "チェック項目2",
+      tip: "補足事項2",
+    },
+    {
+      id: 3,
+      label: "チェック項目3",
+      tip: "補足事項3",
+    },
+  ],
+};
 
 function App() {
   return (
     <div className="p-2 w-80">
       <ul className="menu bg-base-200 rounded-box">
-        <li className="menu-title items-center">チェックリスト</li>
-        {items.map((item) => (
+        <li className="menu-title items-center">{checkList.title}</li>
+        {checkList.items.map((item) => (
           <li>
             <ToggleWithLabel {...item} />
           </li>
